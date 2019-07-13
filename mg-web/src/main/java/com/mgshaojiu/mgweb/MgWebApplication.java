a3aea5a6461925608f692e9a26cf84f9ace52f5f
loli <loligafeo@gmail.com>
2019-07-13 14:54:09 +0800

    Init

---
+++ mg-web/src/main/java/com/mgshaojiu/mgweb/MgWebApplication.java
@@ -0,0 +1,13 @@
+package com.mgshaojiu.mgweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MgWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MgWebApplication.class, args);
+    }
+
+}
